aaada78917f45f25216b9a8846cec1290df5b5e0
Paulo <pauloptgameryt@gmail.com>
2026-05-19 20:07:06 +0100

    refactor: clean up package.json and vite.config.js by removing unused dependencies and code

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,4 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
   },
-})import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
 })
