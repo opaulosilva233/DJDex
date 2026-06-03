70aaa54e3df956bd720ed36527046704ec2aee62
Paulo <pauloptgameryt@gmail.com>
2026-06-03 20:21:55 +0100

    feat: add artisan command to create new admin users

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('make:admin {name} {email} {password}', function ($name, $email, $password) {
+    $user = \App\Models\User::create([
+        'name' => $name,
+        'email' => $email,
+        'password' => \Illuminate\Support\Facades\Hash::make($password),
+    ]);
+    
+    $this->info("Utilizador criado com sucesso: {$user->name} ({$user->email})");
+})->purpose('Criar um novo utilizador administrador');
